8dd3d3db68ce103ec5978c51541a9e0851d2ddb7
Elizabeth Andrews <elizabeth.andrews@jayway.com>
2014-09-29 12:06:20 +0200

    Initial checkin

---
+++ IISWithCertificatesSample.WebApi/Controllers/CatsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace IISWithCertificatesSample.WebApi.Controllers
+{
+    public class CatsController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok("A lot of cats meowing for food");
+        }
+    }
+}
